f8b55991c5e26a95318cc48cb2858595c26083b5
mustafa ilhan <ilhan.mstf@gmail.com>
2020-02-27 18:17:28 +0300

    Add form test cases

---
+++ __tests__/form.test.js
@@ -0,0 +1,16 @@
+describe('Design is Dead - Layout', () => {
+  beforeAll(async () => {
+    await page.setViewport({ width: 1280, height: 768 })
+    await page.goto('https://designisdead.com/')
+  })
+
+  it('should show error message when fields are empty', async () => {
+    const contact = await page.waitForSelector(".Navigation-link.Navigation-contact")
+    await contact.click()
+    const submit = await page.waitForSelector("#_form_10_submit")
+    await submit.click()
+    const className = await page.evaluate(() => document.querySelector('input[name=fullname]').className);
+    
+    expect(className).toEqual(expect.stringContaining('_has_error'))
+  })
+})
